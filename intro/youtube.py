d721a9090f51babe5506ea8a2e74843769434e2b
yoshiking <yoshiking0711@gmail.com>
2018-12-25 13:04:13 +0900

    create intro/youtube.py

---
+++ intro/youtube.py
@@ -0,0 +1,12 @@
+import json
+from urllib.request import urlopen
+
+url = "https://raw.githubusercontent.com/koki0702/introducing-python/master/dummy_api/youTube_top_rated.json"
+
+response = urlopen(url)
+contents = response.read()
+text = contents.decode('utf8')
+data = json.loads(text)
+
+for video in data['feed']['entry'][0:6]:
+    print(video['title']['$t'])
